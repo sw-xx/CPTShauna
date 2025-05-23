3eb42a5320f7d54083891c1f93c1ebedce58ee47
sw-xx <sh.m.wong26@gmail.com>
2025-05-23 10:51:30 -0400

    added more prints (2 lines)

--- repotest.java
+++ repotest.java
@@ -4,5 +4,7 @@ import arc.*;
 			Console con = new Console();
 			
 			con.println("hello world");
+			con.println("thank god its friday");
+			con.println("never get the blackberry yuzu at timmies = tylenol");
 		}
 	}
